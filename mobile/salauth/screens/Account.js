248b7971c06640e03e0b6f56fe83e0e822dd680a
Mauricio Munoz <mau13-13@hotmail.com>
2022-11-22 12:02:40 -0800

    Add camera button, not working

--- mobile/salauth/screens/Account.js
+++ mobile/salauth/screens/Account.js
@@ -1,12 +1,90 @@
 import React from 'react';
-import { Image, Dimensions, Button, SafeAreaView, Text, StyleSheet } from 'react-native';
+import { Image, Dimensions, Button, SafeAreaView, Text, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Camera, CameraType } from 'expo-camera';
+
+function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
 
 function Account({ navigation }) {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [cameraRef, setCameraRef] = useState(null)
+    const [type, setType] = useState(Camera.Constants.Type.back);
+  
+    useEffect(() => {
+      (async () => {
+        const { status } = await Camera.requestCameraPermissionsAsync();
+        setHasPermission(status === 'granted');
+      })();
+    }, []);
+  if (hasPermission === null) {
+      return <View />;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+
+    // https://articles.wesionary.team/camera-module-in-react-native-with-expo-camera-3b8c9f3cd076
+    
     return (
         <SafeAreaView>
             <Text style={styles.comingSoon}>Your Services Coming Soon!</Text>
+            <View style={{ flex: 1 }}>
+            <Camera style={{ flex: 1 }} type={type} ref={ref => {
+                setCameraRef(ref) ;
+            }}>
+                <View
+                style={{
+                    flex: 1,
+                    backgroundColor: 'transparent',
+                    justifyContent: 'flex-end'
+                }}>
+                <TouchableOpacity
+                    style={{
+                    flex: 0.1,
+                    alignSelf: 'flex-end'
+                    }}
+                    onPress={() => {
+                    setType(
+                        type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                    );
+                    }}>
+                    <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{alignSelf: 'center'}} onPress={async() => {
+                    if(cameraRef){
+                    let photo = await cameraRef.takePictureAsync();
+                    console.log('photo', photo);
+                    }
+                }}>
+                    <View style={{ 
+                    borderWidth: 2,
+                    borderRadius:"50%",
+                    borderColor: 'white',
+                    height: 50,
+                    width:50,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'}}
+                    >
+                    <View style={{
+                        borderWidth: 2,
+                        borderRadius:"50%",
+                        borderColor: 'white',
+                        height: 40,
+                        width:40,
+                        backgroundColor: 'white'}} >
+                    </View>
+                    </View>
+                </TouchableOpacity>
+                </View>
+            </Camera>
+            </View>
         </SafeAreaView>
     );
 }
